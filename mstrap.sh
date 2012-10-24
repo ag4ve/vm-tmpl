91dad3ab17f0552cce76805c8e266766241baf06
Shawn Wilson <ag4ve.us@gmail.com>
2012-10-24 04:01:21 -0400

    mapper logic

--- mstrap.sh
+++ mstrap.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-QCOW=./template.qcow2
+DISK=./template.raw
 SIZE=10G
 FSTYPE=ext4
+MAPPER=sda
 
 # mount point has to conform to schroot.conf
 CHROOT=/mnt/chroot
 
 if [ ! -z $1 ]; then
-    QCOW=$1
+    DISK=$1
 fi
 if [ ! -z $2 ]; then
     SIZE=$2
@@ -16,8 +17,11 @@ fi
 if [ ! -z $3 ]; then
     FSTYPE=$3
 fi
+if [ ! -z $4 ]; then
+    MAPPER=$4
+fi
 
-echo "File: $QCOW"
+echo "File: $DISK"
 echo "Size: $SIZE"
 echo "FS type: $FSTYPE"
 echo "Mount point: $CHROOT"
@@ -28,8 +32,8 @@ if [ $UID -eq 0 ]; then
     exit 1;
 fi
 
-if [ -e $QCOW ]; then
-    echo "QCOW file already exists";
+if [ -e $DISK ]; then
+    echo "DISK file already exists";
     exit 1;
 fi
 
@@ -39,46 +43,83 @@ if mountpoint -q $CHROOT; then
 fi
 
 echo "Creating qcow2 image...."
-qemu-img create -f qcow2 $QCOW $SIZE
+dd if=/dev/zero of=$DISK $SIZE
 
 echo "Superuser will be required for some things, hopefully we can cache the password long enough"
 sudo -v
 
-echo "Loading NBD kernel module"
-if ! echo "modprobe nbd > /dev/null 2>&1" | sudo sh; then
-    echo "Failed to load NBD module. Exiting.";
-    exit 1;
-fi
+echo "Partitioning $DISK"
+parted -s $DISK mklabel msdos
+parted -s --align=none $DISK mkpart primary 64s 100%
 
-for NBDDEV in $(find /dev -name "nbd?"); do 
-    echo "Testing $NBDDEV"; 
-    if [ -z $(nbd-client -c $NBDDEV) ]; then 
-        echo "$NBDDEV OK"; 
+for BLOCK in $(find /dev -name "loop[0-9]*"); do 
+    echo "Testing $BLOCK"; 
+    if echo "losetup $BLOCK > /dev/null 2>&1" | sudo sh; then 
+        echo "$BLOCK OK"; 
         break; 
     fi 
 done
 
-if [ -z $NBDDEV ]; then
+if [ -z $BLOCK ]; then
     echo "No unused NBD device found. Exiting.";
     exit 1;
 fi
 
-echo "Starting NBD daemon"
-sudo qemu-nbd --nocache -c $NBDDEV $QCOW
-
-echo "Partitioning and formatting device."
-echo "echo \",,L,*\" | sfdisk -D $NBDDEV" | sudo sh
-sudo mkfs -t ext4 $NBDDEV
-
-sudo mkdir -p $CHROOT
-if ! echo "mount -o uid=$UID $NBDDEV $CHROOT" | sudo sh; then
-    echo "Failed to mount $NBDDEV on $CHROOT. Exiting.";
+echo "Using loop device $BLOCK"
+if ! sudo losetup $BLOCK $DISK; then
+    echo "Failed to setup loop device";
     exit 1;
 fi
 
-mkdir $CHROOT/{dev,proc,sys}
-mkdir -p $CHROOT/boot/grub
-mkdir -p $CHROOT/root/.ssh
+#   logical_start_sector num_sectors target_type target_args
+# the formula for num_sectors is <size in gigs> * 1024^3 (gig in bytes) / 512 . 512 because:
+#   Devices are created by loading a table that specifies a target 
+#   for each sector (512 bytes) in the logical device.
+echo "Creating partition table";
+if ! echo "echo \"0 $[$SIZE*2097152] linear $BLOCK 0\" | dmsetup create $MAPPER > /dev/null 2>&1" \
+        | sudo sh; then
+    echo "Failed to create device $MAPPER mapper for $BLOCK";
+    exit 1;
+fi
+
+echo "Creating mapper device for partition";
+if ! echo "kpartx -a /dev/mapper/$MAPPER" | sudo sh; then
+    echo "Failed to create device for the /dev/mapper/$MAPPER partition";
+    exit 1;
+fi
+
+echo "Copying mapper device to /dev/m$MAPPER";
+if [ -e /dev/m$MAPPER ] || [ -e /dev/m$MAPPER\1 ]; then
+    echo "/dev/m$MAPPER or /dev/m$MAPPER\1 exist";
+    exit 1;
+else
+    sudo ln -s $(readlink -f /dev/mapper/$MAPPER) /dev/m$MAPPER;
+    sudo ln -s $(readlink -f /dev/mapper/$MAPPER\1) /dev/m$MAPPER\1;
+fi
+
+echo "Creating filesystem";
+if ! echo "mkfs -t $FSTYPE /dev/m$MAPPER\1 > /dev/null 2>&1" | sudo sh; then
+    echo "Failed to create $FSTYPE on /dev/m$MAPPER";
+    exit 1;
+fi
+
+if [ -e $CHROOT ]; then
+    echo "$CHROOT already exists. Moving on... ";
+else
+    if ! echo "mkdir -p $CHROOT" | sudo sh; then
+        echo "Failed to create chroot directory $CHROOT";
+        exit 1;
+    fi
+fi
+
+if ! echo "mount -o uid=$UID /dev/m$MAPPER\1 $CHROOT" | sudo sh; then
+    echo "Failed to mount $BLOCK on $CHROOT. Exiting.";
+    exit 1;
+fi
+
+sudo mkdir $CHROOT/{dev,proc,sys}
+sudo mkdir -p $CHROOT/boot/grub
+sudo mkdir -p $CHROOT/root/.ssh
 
 # schroot should take care of this, but i haven't looked into how to get
 # multistrap to run inside schroot
@@ -87,11 +128,11 @@ sudo mount -t sysfs sysfs $CHROOT/sys
 sudo mount --bind /dev $CHROOT/dev
 sudo mount -t devpts devpts $CHROOT/dev/pts
 
-multistrap -f ./multistrap.config
+sudo multistrap -f ./multistrap.config
 
-rm -f $CHROOT/etc/resolv.conf
-rm -f $CHROOT/etc/ssh/*key*
-echo $(cat $HOME/.ssh/id_dsa.pub) $USER\@$HOST > $CHROOT/root/.ssh/authorized_keys
+sudo rm -f $CHROOT/etc/resolv.conf
+sudo rm -f $CHROOT/etc/ssh/*key*
+sudo echo $(cat $HOME/.ssh/id_dsa.pub) $USER\@$HOST > $CHROOT/root/.ssh/authorized_keys
 cp /etc/apt/apt.conf $CHROOT/etc/apt/apt.conf 2> /dev/null
 
 echo -n         > $CHROOT/etc/network/interfaces
@@ -102,14 +143,14 @@ echo "Etc/UTC"  > $CHROOT/etc/timezone
 
 schroot -d / -c multistrap locale-gen en_US.UTF-8
 schroot -d / -c multistrap dpkg-reconfigure -f noninteractive -a
-schroot -d / -c multistrap grub-install $NBDDEV
+schroot -d / -c multistrap grub-install $BLOCK
 schroot -d / -c multistrap update-grub
 
 
 echo "Unmounting"
 sudo umount $CHROOT
 echo "Breaking down NBD"
-sudo nbd-client -d $NBDDEV
+sudo nbd-client -d $BLOCK
 
 echo "Everything is done"
 
